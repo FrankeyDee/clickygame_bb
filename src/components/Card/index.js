1a833c8b90e54b8d1eba1534e11537a2ea117c87
frankey darban <frankeydarban@gmail.com>
2019-10-26 01:49:48 -0700

    added data to json file

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,9 +3,9 @@ import './style.css';
 
 function Card(props) {
     return (
-        <div className='card border-primary mb-3'>
-                <img onClick={() => props.clickCard(props.id)} id={props.id} alt={props.name} src={props.image} />
-            </div>
+        <div className='card border-primary mb-3' onClick={() => props.clickCard(props.id)} id={props.id} alt={props.name} src={props.image} >
+            <img className='image' src='{( props.image)}' />
+        </div>
     )
 }
 
