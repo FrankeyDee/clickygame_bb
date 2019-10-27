096e1edffcd6f5fd75d24251eb8dafea20fbff0d
frankey darban <frankeydarban@gmail.com>
2019-10-26 23:46:41 -0700

    added shuffle of cards

--- src/App.js
+++ src/App.js
@@ -5,23 +5,45 @@ import Card from './components/Card';
 import Wrapper from './components/Wrapper';
 import cards from './cards.json';
 
-class App extends Component {
-  this.state = {
-
+class App extends React.Component {
+  state = {
+    cards: cards,
+    score: 0,
+    highscore: 0,
+    selected: [],
   };
 
 clickCard = id => {
-    // const guessed = this.state.guessed.filter
-    // };
-  //   //SHUFFLE CARDS
-  //   //ADD TO SCORE
+  if (!this.state.cards.includes(id)) {
+    let selected = this.state.selected
+    selected.push(id)
+    let score = this.state.score + 1
+    this.setState({ selected, score, message: "Correct!" })
+    if (score > this.state.highScore) {
+      let highScore = score
+      this.setState({ highScore })
+    }
+    if (score === this.state.cards.length) {
+      this.setState({
+        score: 0,
+        selected: [],
+        message: "PERFECT SCORE! Click to start over"
+      })
+    }
+  } else {
+      this.setState({
+        score: 0,
+        cards: [],
+        message: "OH NO! You've already clicked this!"
+      })
+  }
 };
 
   render(){
     return (
       <Wrapper>
         <Title>Clicky Game</Title>
-        {this.state.cards.map(card => (
+        {this.state.cards.sort(() => .5 - Math.random()).map(card => (
         <Card
           clickCard={this.clickCard}
           id={card.id}
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 function Card(props) {
     return (
-        <div className='card border-primary mb-3' onClick={() => props.clickCard(props.id)} id={props.id} alt={props.name} src={props.image} >
+        <div className='card bbcard' onClick={() => props.clickCard(props.id)} id={props.id} alt={props.name} src={props.image} >
             <img className='image' src={props.image} />
         </div>
     )
